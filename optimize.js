963d60c0a5b6a778541afe4b5c78effcbb75b6a7
Marc Anton Dahmen <mail@marcdahmen.de>
2020-10-31 10:43:42 +0100

    Update optimize.js

--- optimize.js
+++ optimize.js
@@ -1,6 +1,7 @@
 const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 
+// Taken from https://coderrocketfuel.com/article/compress-a-png-image-size-by-up-to-75-percent-with-node-js
 (async () => {
 	const files = await imagemin(['temp/*.png'], {
 		destination: 'temp/optimized',
