b7f71b4f2e08fdb08c17f1e82c33499857c517eb
Marc Anton Dahmen <mail@marcdahmen.de>
2025-04-15 09:19:16 +0200

    improve gallery

--- build.sh
+++ build.sh
@@ -1,34 +1,78 @@
 #!/bin/bash
 
 files=$(git log --pretty='' --name-only | awk '!seen[$0]++' | grep -E '.*(\.png|\.mp4|\.webp)$')
-baseUrl="https://raw.githubusercontent.com/marcantondahmen/media-files/master"
-output="public/index.html"
+directories=$(ls -d */ | grep -v temp | grep -v public | grep -v node_modules)
+baseUrl='https://raw.githubusercontent.com/marcantondahmen/media-files/master'
+template=$(<template.html)
+public='./public'
 
 mkdir -p public
 
-gallery=''
+homeLink() {
+	cls=''
 
-for file in $(echo $files); do
-	url="$baseUrl/$file"
-	element="<img src=\"${url}\" />"
-
-	if [[ $file =~ \.mp4$ ]]; then
-		element="<video playsinline autoplay muted><source src=\"${url}\" type=\"video/mp4\" /></video>"
+	if [ -z "$1" ]; then
+		cls='class="active"'
 	fi
 
-	read -r -d '' item <<-EOF
-		<a
-			href="${url}" 
-			title="${file}"
-			target="_blank"
-		>
-			${element} 
-		</a>
-	EOF
+	echo "<a href=\"./\" $cls>all</a>"
+}
 
-	gallery="${gallery}\n${item}\n"
+generateGallery() {
+	page="${1%/}"
+	nav="$(homeLink $1)"
+	gallery=''
+	filtered=$(echo "$files" | grep "$1")
+
+	if [ -z "$page" ]; then
+		page='index'
+	fi
+
+	for dir in $directories; do
+		cls=''
+
+		if [[ "$1" == "$dir" ]]; then
+			cls='class="active"'
+		fi
+
+		read -r -d '' navItem <<-EOF
+			<a href="./${dir%/}.html" $cls>
+				 ${dir%/}
+			</a>
+		EOF
+
+		nav="${nav}\n${navItem}"
+	done
+
+	templateWithNav="${template/__NAV__/$nav}"
+
+	for file in $(echo $filtered); do
+		url="$baseUrl/$file"
+		element="<img src=\"${url}\" />"
+
+		if [[ $file =~ \.mp4$ ]]; then
+			element="<video playsinline autoplay muted><source src=\"${url}\" type=\"video/mp4\" /></video>"
+		fi
+
+		read -r -d '' item <<-EOF
+			<a
+				href="${url}" 
+				class="card"
+				title="${file}"
+				target="_blank"
+			>
+				${element} 
+			</a>
+		EOF
+
+		gallery="${gallery}\n${item}\n"
+	done
+
+	echo -e "${templateWithNav/__ITEMS__/$gallery}" >"$public/$page.html"
+}
+
+generateGallery
+
+for dir in $directories; do
+	generateGallery $dir
 done
-
-html=$(<template.html)
-
-echo -e "${html/__ITEMS__/$gallery}" >$output
