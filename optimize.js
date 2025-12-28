27e3edc8bbb3f53cd72feb6feecbcdfde36e9648
Marc Anton Dahmen <mail@marcdahmen.de>
2025-12-28 20:28:11 +0100

    export webp and png in optimize.js

--- optimize.js
+++ optimize.js
@@ -1,5 +1,6 @@
 import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
+import imageminPngquant from 'imagemin-pngquant';
 
 (async () => {
 	await imagemin(['temp/*.png'], {
@@ -7,6 +8,14 @@ import imageminWebp from 'imagemin-webp';
 		plugins: [imageminWebp({ quality: 70 })],
 	});
 
+	await imagemin(['temp/*.png'], {
+		destination: 'temp/optimized',
+		plugins: [
+			imageminPngquant({
+				quality: [0.7, 1.0],
+			}),
+		],
+	});
+
 	console.log('Images optimized');
 })();
-
